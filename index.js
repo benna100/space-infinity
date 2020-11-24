80986b592fda8ecdb34bba60775bb9dda405872e
behuFTP <benjamin.dals.hughes@gmail.com>
2020-11-24 15:45:29 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,32 @@
+console.log("script initiated");
+
+let pointsSpan = document.querySelector("span.points");
+console.log(pointsSpan);
+
+let points = 0;
+let spacebarDown = false;
+document.addEventListener("keydown", (event) => {
+  if (event.code === "Space") {
+    spacebarDown = true;
+  }
+});
+
+document.addEventListener("keyup", (event) => {
+  if (event.code === "Space") {
+    spacebarDown = false;
+    points = 0;
+    renderPoints(points);
+  }
+});
+
+setInterval(() => {
+  if (spacebarDown) {
+    points++;
+  }
+
+  renderPoints(points);
+}, 1000);
+
+function renderPoints(points) {
+  pointsSpan.innerHTML = points;
+}
